d629471b47a1b1ebc29a57f6618726fce6ddfb43
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-08 22:06:39 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    songs.artist
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(song)
+    song = Song.new(name,artist)
+  end
+
+  def self.song_count
+    @songs.count
+  end
+
+
+end
